4122b6f1b7b8e3cc3669d90f1cc49f05ee0f90b7
Allan F. Gagnon <allan.gagnon@gmail.com>
2023-05-01 08:23:29 -0500

    Create blink.py
    Added file blink.py for micropython

---
+++ blink.py
@@ -0,0 +1,12 @@
+from machine import Pin
+import utime
+led = Pin(6, Pin.OUT)
+
+while True:
+    led.value(1)
+    print("ON")
+    utime.sleep(1)
+    
+    led.value(0)
+    print("OFF")
+    utime.sleep(1)
